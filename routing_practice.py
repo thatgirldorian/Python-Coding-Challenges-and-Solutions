e04d0b2571bbb37dd985617db0bfa97fe79b498b
Debbie Otuagomah <debbie.otua@gmail.com>
2021-10-12 01:21:52 +0100

    Update routing_practice.py

--- routing_practice.py
+++ routing_practice.py
@@ -1,8 +1,11 @@
-from flask import Flask  # Import Flask to allow us to create our app
+# Import Flask to allow us to create our app
+from flask import Flask  
 
-app = Flask(__name__)    # Create a new instance of the Flask class called "app"
+# Create a new instance of the Flask class called "app"
+app = Flask(__name__)    
 
-@app.route('/')          # The "@" decorator associates this route with the function immediately following
+# The "@" decorator associates this route with the function immediately following
+@app.route('/')          
 def hello_world():
     return 'Hello World!'  # Return the string 'Hello World!' as a response
 
