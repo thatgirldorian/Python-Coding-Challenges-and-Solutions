d25a72600d4a54d05f1454bdf18e8aa9ceacb2f1
Debbie Otuagomah <thatgirldorian@gmail.com>
2021-10-31 23:40:27 +0100

    Add files via upload

--- users_crud/server.py
+++ users_crud/server.py
@@ -1,4 +1,7 @@
+from datetime import datetime
+
 from flask import Flask, render_template, request, redirect
+from werkzeug.exceptions import default_exceptions
 
 # import the class from user.py
 from user import User
@@ -14,7 +17,7 @@ def users():
     # call the get all classmethod to get all users
     users = User.get_all()
     print(users)
-    return render_template("index.html", users=users)
+    return render_template("index.html", users=users, default=datetime.utcnow)
 
 @app.route('/create_user')
 def new():
@@ -25,7 +28,40 @@ def create_user():
     print(request.form)
     User.save(request.form)
     return redirect('/')
-    # Don't forget to redirect after saving to the database.
+# Don't forget to redirect after saving to the database.
+
+#edit person with their id
+@app.route('/edit/<int:id>')
+def edit(id):
+    data ={ 
+        "id":id
+    }
+    return render_template("edit.html",user=User.get_one(data))
+
+#show user iformation when id is passed
+@app.route('/show/<int:id>')
+def show(id):
+    data ={ 
+        "id":id
+    }
+    return render_template("show.html",user=User.get_one(data))
+
+
+@app.route('/update',methods=['POST'])
+def update():
+    User.update(request.form)
+    return redirect('/users')
+
+
+#delete people when their id is passed
+@app.route('/destroy/<int:id>')
+def destroy(id):
+    data ={
+        'id': id
+    }
+    User.destroy(data)
+    return redirect('/users')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
--- users_crud/user.py
+++ users_crud/user.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 # import the function that will return an instance of a connection
 from mysqlconnection import connectToMySQL
 # model the class after the friend table from our database
@@ -9,7 +11,9 @@ class User:
         self.email = data['email']
         self.created_at = data['created_at']
         self.updated_at = data['updated_at']
-    # Now we use class methods to query our database
+
+        
+    # Now we use class methods to query our database and return users on the Read All page 
     @classmethod
     def get_all(cls):
         query = "SELECT * FROM users;"
@@ -22,8 +26,28 @@ class User:
             users.append( cls(user) )
         return users
 
+    #this will create a new user ane add them to the database
     @classmethod
     def save(cls, data ):
         query = "INSERT INTO users ( first_name , last_name , email , created_at, updated_at ) VALUES ( %(fname)s , %(lname)s , %(email)s , NOW() , NOW() );"
         # data is a dictionary that will be passed into the save method from server.py
         return connectToMySQL('users_schema').query_db( query, data )
+        
+    #this will select a particulat ID from the database for the actions to work
+    @classmethod
+    def get_one(cls,data):
+        query  = "SELECT * FROM users WHERE id = %(id)s";
+        result = connectToMySQL('users_schema').query_db(query,data)
+        return cls(result[0])
+
+    #this will update a user and return an updated webpage with new/updated info
+    @classmethod
+    def update(cls,data):
+        query = "UPDATE users SET first_name=%(first_name)s,last_name=%(last_name)s,email=%(email)s,updated_at=NOW() WHERE id = %(id)s;"
+        return connectToMySQL('users_schema').query_db(query,data)
+
+    #this will delete a user and then return an up
+    @classmethod
+    def destroy(cls,data):
+        query  = "DELETE FROM users WHERE id = %(id)s;"
+        return connectToMySQL('users_schema').query_db(query,data)
