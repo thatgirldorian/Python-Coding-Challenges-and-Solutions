69d0152145d73a3f60749ddb0f77421ca4399274
Debbie Otuagomah <thatgirldorian@gmail.com>
2021-11-10 23:16:30 +0100

    Add files via upload

---
+++ Survey/flask_app/controllers/surveys.py
@@ -0,0 +1,21 @@
+from flask import render_template, request, redirect
+from flask_app import app
+from flask_app.models.survey import Survey
+
+#Have the root route render the form
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+#Have the "/result" route display the information from the form on a new HTML page
+@app.route('/create/survey',methods=['POST'])
+def create_survey():
+    #validate here with the class
+    if Survey.is_valid(request.form):
+        Survey.save(request.form)
+        return redirect('/result')
+    return redirect('/')
+
+@app.route('/result')
+def result():
+    return render_template('result.html', survey = Survey.get_last_survey())
---
+++ Survey/flask_app/models/survey.py
@@ -0,0 +1,42 @@
+from flask_app.config.mysqlconnection import connectToMySQL
+from flask import flash
+
+#connecting the form to the database
+class Survey:
+    def __init__(self, data):
+        self.id = data['id']
+        self.name = data['name']
+        self.location = data['location']
+        self.language = data['language']
+        self.comment = data['comment']
+        self.created_at = data['created_at']
+        self.updated_at = data['updated_at']
+
+    @classmethod
+    def save(cls, data):
+        query = "INSERT INTO dojos (name, location, language, comment, created_at, updated_at) VALUES ( %(name)s, %(location)s, %(language)s, %(comment)s, NOW(), NOW());"
+        return connectToMySQL('survey_schema').query_db(query, data)
+
+    @classmethod
+    def get_last_survey(cls):
+        query = "SELECT * FROM dojos ORDER BY dojos.id DESC LIMIT 1;"
+        results = connectToMySQL('survey_schema').query_db(query)
+        return Survey(results[0])
+
+    #basic validation for our form
+    @staticmethod
+    def is_valid(survey):
+        is_valid = True # we assume this is true
+        if len(survey['name']) < 3:
+            is_valid = False
+            flash("Name must be at least 3 characters.")
+        if len(survey['location']) < 1:
+            is_valid = False
+            flash("Must choose a dojo location.")
+        if len(survey['language']) < 1:
+            is_valid = False
+            flash("Must choose a programming language.")
+        if len(survey['comment']) < 3:
+            is_valid = False
+            flash("Comment must be at least 3 characters.")
+        return is_valid
