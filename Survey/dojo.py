751feb1b2778504b779c48156c539f6b6ff9483e
Debbie Otuagomah <debbie.otua@gmail.com>
2021-11-10 23:15:46 +0100

    Delete Survey directory

--- Survey/dojo.py
+++
@@ -1,8 +0,0 @@
-from mysqlconnection import connectToMySQL
-
-class Dojo:
-
-    @classmethod
-    def save(cls, data):
-        query = "INSERT INTO dojos (name, location, language, comment, created_at, updated_at) VALUES ( %(name)s, %(location)s, %(language)s, %(comment)s, NOW(), NOW());"
-        return connectToMySQL('survey_schema').query_db(query, data)
